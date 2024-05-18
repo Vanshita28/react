d33173df7f97ae31a03542f67c403e08ae09c0ff
Vanshita Sharma <vanshi2820sharma@gmail.com>
2024-05-18 15:40:35 +0530

    added codes

---
+++ src/component/Greet.js
@@ -0,0 +1,23 @@
+export default function greet (){
+ return (
+    <div>
+            <h2>
+                hello
+            </h2>
+    </div>
+ )
+}
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
